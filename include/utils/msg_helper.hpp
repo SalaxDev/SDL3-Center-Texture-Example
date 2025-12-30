74d3814d83b90480b85b818f39934df9f4caef5a
SalaxDev <191417878+SalaxDev@users.noreply.github.com>
2025-12-30 15:37:02 +0330

    Added throw functionality

---
+++ include/utils/msg_helper.hpp
@@ -0,0 +1,10 @@
+#ifndef MSG_HELPER_HPP
+#define MSG_HELPER_HPP
+
+#include "type_aliases.hpp"
+
+namespace msg {
+    void logerr(cstrc name, cstrc cause, cbool shouldThrow = false) ;
+}
+
+#endif // MSG_HELPER_HPP
