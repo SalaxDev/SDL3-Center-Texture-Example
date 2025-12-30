a9d9330afea4adc12720bd360c61544b640943c8
SalaxDev <191417878+SalaxDev@users.noreply.github.com>
2025-12-30 15:18:29 +0330

    Added log error helper

---
+++ include/utils/type_aliases.hpp
@@ -0,0 +1,14 @@
+#ifndef TYPE_ALIASES_HPP
+#define TYPE_ALIASES_HPP
+
+// Aliases
+using uint = unsigned int ;
+using cuint = const unsigned int ;
+using cint = const int ;
+using cfloat = const float ;
+using cdouble = const double ;
+using cstr = const char * ;
+using cstrc = const char * const ;
+using cbool = const bool ;
+
+#endif // TYPE_ALIASES_HPP
