8bb3710da5af8bc7da823fb78ac2e03b420973fb
SalaxDev <191417878+SalaxDev@users.noreply.github.com>
2026-01-04 16:01:12 +0330

    Added better function running

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 
 int main(int argc, const char** argv) {
     wcWindow window({800, 600}, "Center Texture") ;
-    window.run() ;
+    window.setFramerate(144)
+    .setTextureTextColor({200, 200, 200, 255})
+    .init().run() ;
     
     return 0 ;
 }
